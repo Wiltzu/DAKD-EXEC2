bc8b95158349f572b63e8ab1d39db10d7aa0bc37
haapis <ylhaisyys@gmail.com>
2013-10-17 16:49:41 +0300

    testi

--- task2.py
+++ task2.py
@@ -2,11 +2,23 @@ import task1
 from sklearn import tree
 import numpy as np
 
+def separate_classifiers(data):
+	res_data = []
+	cls = []
+	for row in data:
+		res_data.append(row[:1])
+		cls.append(row[4])
+	return res_data, cls
 
-
+def decision_tree(data, cls):
+	classifier = tree.DecisionTreeClassifier().fit(data, cls)
+	print type(classifier)
 
 def main():
-	data = task1.get_full_data()
+	original_data = task1.get_full_data()
+	data, classifier = separate_classifiers(original_data)
+	decision_tree(data, classifier)
+
 
 if __name__ == '__main__':
 	main()
