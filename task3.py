b955baa4da3b9b89ee8b0917ff26dad01d4b28e3
haapis <ylhaisyys@gmail.com>
2013-10-24 21:01:44 +0300

    bayes visualisointi

--- task3.py
+++ task3.py
@@ -5,19 +5,35 @@ from sklearn.naive_bayes import GaussianNB as nb
 from sklearn import tree
 
 def cross_val(data, classifiers):
-    bayes_estimates = []
-    tree_estimates =[]
-    data = np.array(data)
-    classifiers = np.array(classifiers)
-    folds = cross_validation.KFold(len(classifiers), n_folds=10, shuffle=True)
-    for train, test in folds:
-        bayes = naive_bayes(data[train], classifiers[train])
-        bayes_estimates.append(bayes.score(data[test], classifiers[test]))
-        tree = decision_tree(data[train], classifiers[train])
-        tree_estimates.append(tree.score(data[test], classifiers[test]))
+    bayes_averages =  []
+    tree_averages = []
+    for i in range(5):
+        bayes_estimates = []
+        tree_estimates =[]
+        data = np.array(data)
+        classifiers = np.array(classifiers)
+        folds = cross_validation.KFold(len(classifiers), n_folds=10, shuffle=True)
+        for train, test in folds:
+            bayes = naive_bayes(data[train], classifiers[train])
+            bayes_estimates.append(bayes.score(data[test], classifiers[test]))
+            tree = decision_tree(data[train], classifiers[train])
+            tree_estimates.append(tree.score(data[test], classifiers[test]))
 
-    print 'Naive bayes %s' %str(np.mean(bayes_estimates))
-    print 'decision tree %s' %str(np.mean(tree_estimates))
+
+        bayes_averages.append(np.mean(bayes_estimates))
+        tree_averages.append(np.mean(tree_estimates))
+
+def visualize(bayes_avg, tree_avg):
+    columns = ['Bayes scores', 'Tree scores']
+    ax = mplot.subplot(111,frame_on=False)
+    ax.xaxis.set_visible(False)
+    ax.yaxis.set_visible(False)
+    cells = [bayes_avg, tree_avg]
+    table = mplot.table(cellText=cells,
+                        colLabels=columns,
+                        loc='center')
+    mplot.subplots_adjust(left=0.2)
+    mplot.show()
 
 def naive_bayes(data, classifiers):
     bayes = nb()
