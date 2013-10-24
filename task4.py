d5b8aca83759184c30664fd800ce8c45317f3d13
Wiltzu <ville.ahti2@gmail.com>
2013-10-24 18:27:11 +0300

    task 4 kokeilua

--- task4.py
+++ task4.py
@@ -1,10 +1,13 @@
-from sklearn.cluster import KMeans as km
+from sklearn.cluster import KMeans
 import numpy as np
 import task1, task2
 
 
 
-
+def k_means(data):
+	for i in range(10)[1:]:
+		km = KMeans(n_clusters = i).fit(data)
+		print km.score(data)
 
 
 
@@ -15,6 +18,7 @@ import task1, task2
 def main():
 	original_data = task1.get_full_data()
 	data, classifier = task2.separate_classifiers(original_data)
+	k_means(data)
 
 if __name__ == '__main__':
 	main()
