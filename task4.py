38c530009827c2c355bbfa05f7a6cb88330b15cd
Haapis <ylhaisyys@gmail.com>
2013-10-24 19:02:23 +0300

    task4 valmis

--- task4.py
+++ task4.py
@@ -1,24 +1,31 @@
 from sklearn.cluster import KMeans
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+import pylab as pl
 import task1, task2
 
 
 
 def k_means(data):
-	for i in range(10)[1:]:
-		km = KMeans(n_clusters = i).fit(data)
-		print km.score(data)
+    data = np.array(data)
+    for i in [3,5,8,10]:
+        name = 'k=%d'%i
+        km = KMeans(n_clusters = i).fit(data)
+        figure = pl.figure(name, figsize=(8,6))
+        pl.clf()
+        ax = Axes3D(figure, rect=[0, 0, .95, 1], elev=48, azim=134)
 
+        pl.cla()
+        labels = km.labels_
+        ax.scatter(data[:,3], data[:,0], data[:,2], c=labels.astype(float))
 
-
-
-
+    pl.show()
 
 
 def main():
-	original_data = task1.get_full_data()
-	data, classifier = task2.separate_classifiers(original_data)
-	k_means(data)
+    original_data = task1.get_full_data()
+    data, classifier = task2.separate_classifiers(original_data)
+    k_means(data)
 
 if __name__ == '__main__':
-	main()
+    main()
